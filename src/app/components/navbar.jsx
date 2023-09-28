589b4d9e268f842303aedb3f65a2179aae941246
anadantasp <caroldantasp@gmail.com>
2023-09-28 16:09:47 -0300

    Separando por componentes

---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,20 @@
+export default function NavBar() {
+    return (
+        <nav className="flex items-center justify-between bg-purple">
+            <div>
+                <h1>Mundos em Papel</h1>
+            </div>
+            <div>
+                <input type="search" name="busca" id="busca" />
+                <ul className="flex gap-4">
+                    <li>Home</li>
+                    <li>Produtos</li>
+                    <li>Categoria</li>
+                </ul>
+            </div>
+            <div className="h-12 w-12 rounded-full overflow-hidden">
+                <img src="https://github.com/anadantasp.png" alt="avatar" />
+            </div>
+        </nav>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,10 @@
 import Image from 'next/image';
+import NavBar from './components/navbar';
 
 export default function Home() {
   return (
-    <nav className="flex items-center justify-between bg-purple">
-      <div>
-        <h1>Mundos em Papel</h1>
-      </div>
-      <div>
-        <input type="search" name="busca" id="busca" />
-        <ul className="flex gap-4">
-          <li>Home</li>
-          <li>Produtos</li>
-          <li>Categoria</li>
-        </ul>
-      </div>
-      <div className="h-12 w-12 rounded-full overflow-hidden">
-        <img src="https://github.com/anadantasp.png" alt="avatar" />
-      </div>
-    </nav>
+    <>
+      <NavBar />
+    </>
   );
 }
